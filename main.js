cb6835b7d29649dd50a0da740d4c6616dec9d4fe
JS29847 <83806602+JS29847@users.noreply.github.com>
2021-09-30 12:38:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+song1 = "";
+song2 = "";
+
+function preload()
+{
+    song1 = loadSound("music1.mp3");
+    song2 = loadSound("music2.mp3");
+}
+
+leftWristX = 0;
+leftWristY = 0;
+rightwristX = 0;
+rightWristY = 0;
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +" rightWristY = "+ leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+    }
+}
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
